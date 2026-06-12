10ad0967f1f3e625caf4f3d8d9158b87978c7fcd
Sergey Sokolovsky <sergey270307@gmail.com>
2026-06-12 20:21:00 +0700

    fix(typing): models checks TYPE_CHECKING valude to avoid runtime issues

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -1,13 +1,21 @@
+from typing import TYPE_CHECKING
 from django.contrib import admin
 from apps.projects.models import Skill, Project
 
+if TYPE_CHECKING:
+    BaseSkillAdmin = admin.ModelAdmin[Skill]
+    BaseProjectAdmin = admin.ModelAdmin[Project]
+else:
+    BaseSkillAdmin = admin.ModelAdmin
+    BaseProjectAdmin = admin.ModelAdmin
+
 @admin.register(Skill)
-class SkillAdmin(admin.ModelAdmin[Skill]):
+class SkillAdmin(BaseSkillAdmin):
     list_display = ("id", "name")
     search_fields = ("name",)
 
 @admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin[Project]):
+class ProjectAdmin(BaseProjectAdmin):
     list_display = ("name", "owner", "status", "created_at")
     list_filter = ("status", "created_at")
     search_fields = ("name", "description", "owner__email")
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,9 +1,15 @@
+from typing import TYPE_CHECKING
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from apps.users.models import User
 
+if TYPE_CHECKING:
+    BaseModelAdmin = admin.ModelAdmin[User]
+else:
+    BaseModelAdmin = admin.ModelAdmin
+
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin[User]):
+class UserAdmin(BaseModelAdmin):
     list_display = ("email", "name", "surname", "phone", "is_staff")
     search_fields = ("email", "name", "surname")
     ordering = ("email",)
