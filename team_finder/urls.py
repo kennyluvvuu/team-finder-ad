1f147ac85974cc8910d990c8ef99ab30d578f364
Sergey Sokolovsky <sergey270307@gmail.com>
2026-06-12 19:45:04 +0700

    chore: admin, fixtures & final wiring

--- team_finder/urls.py
+++ team_finder/urls.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from apps.projects import views as project_views
 
 def redirect_to_list(request):
     return redirect("/project/list/")
@@ -11,7 +12,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("apps.users.urls")),
     path("projects/", include("apps.projects.urls")),
-    path("project/", include("apps.projects.urls")),
+    path("project/list/", project_views.project_list_view, name="project_list_short"),
     path("", redirect_to_list),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
