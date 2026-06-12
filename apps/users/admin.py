a75c6ac82c25b477df930c5e2e80d7af4f9fb364
Sergey Sokolovsky <sergey270307@gmail.com>
2026-06-12 19:41:55 +0700

    feat(users): custom User model & manager

---
+++ apps/users/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from apps.users.models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "name", "surname", "phone", "is_staff")
+    search_fields = ("email", "name", "surname")
+    ordering = ("email",)
---
+++ apps/users/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from apps.users.utils import generate_avatar
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, surname, phone, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            name=name,
+            surname=surname,
+            phone=phone,
+            **extra_fields
+        )
+        if not user.avatar:
+            user.avatar = generate_avatar(name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, surname, phone, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+            
+        return self.create_user(email, name, surname, phone, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=124)
+    surname = models.CharField(max_length=124)
+    avatar = models.ImageField(upload_to="avatars/", blank=True)
+    phone = models.CharField(max_length=12)
+    github_url = models.URLField(blank=True)
+    about = models.TextField(max_length=256, blank=True)
+    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name", "surname", "phone"]
+    
+    def __str__(self):
+        return f"{self.name} {self.surname} ({self.email})"
